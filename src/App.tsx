a2f3f7e3fed5d95362cf3c105109e9aacbae58fb
Romanca <r.jamrisko@gmail.com>
2021-01-22 22:28:31 +0100

    added ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,19 @@ import React from 'react';
 import { TodoList } from './TodoList';
 import { AddTodoForm } from './AddTodoForm';
 import TodoProvider from './TodoProvider';
+import Theme from './ThemeProvider';
 
-function App() {
+const App = () => {
 	return (
 		<TodoProvider>
-			<div>
-				<main style={{ display: 'flex' }}>
-					<section>
-						<AddTodoForm />
-					</section>
-					<section style={{ marginLeft: 20 }}>
-						<header style={{ marginLeft: 10 }}>
-							<h1>Taskslist</h1>
-						</header>
-
-						<TodoList />
-					</section>
-				</main>
-			</div>
+			<Theme>
+				<div>
+					<AddTodoForm />
+					<TodoList />
+				</div>
+			</Theme>
 		</TodoProvider>
 	);
-}
+};
 
 export default App;
