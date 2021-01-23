ca0a3a57ebee4af1898de7d1e9a6f7b0bd32d978
Romanca <r.jamrisko@gmail.com>
2021-01-23 22:39:57 +0100

    creating themes

--- src/AddTodoForm.tsx
+++ src/AddTodoForm.tsx
@@ -1,33 +1,6 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
 import { useTodoActions } from './TodoProvider';
-
-const Input = styled.input`
-	border: 1px solid black;
-	height: 30px;
-	outline: none;
-	border-radius: 5px;
-	width: 80%;
-`;
-
-const Button = styled.button`
-	height: 30px;
-	width: 30px;
-	color: white;
-	background: green;
-	border: none;
-	outline: none;
-	cursor: pointer;
-	border-radius: 50%;
-	font-size: 15px;
-	margin-left: 5px;
-`;
-
-const TodoFormContainer = styled.div`
-	flex-direction: row;
-	width: 350px;
-	padding: 5px;
-`;
+import { TodoFormContainer, GreenButton, Input } from './StyledComponents';
 
 export const AddTodoForm = () => {
 	const { addTodo } = useTodoActions();
@@ -46,7 +19,7 @@ export const AddTodoForm = () => {
 					}}
 				/>
 
-				<Button
+				<GreenButton
 					type='submit'
 					onClick={(e) => {
 						e.preventDefault();
@@ -54,7 +27,7 @@ export const AddTodoForm = () => {
 						setText('');
 					}}>
 					+
-				</Button>
+				</GreenButton>
 			</TodoFormContainer>
 		</form>
 	);
